48e64ad932fa9c93874d65b4e966166e39ccdd4b
J0kerPAnda <tochka313@gmail.com>
2016-11-14 09:58:57 +0300

    Implemented basic database schema

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,35 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User, UserManager
 
-# Create your models here.
+
+class Tag( models.Model ):
+	caption = models.CharField( null = False, blank = False, max_length = 20 )
+
+	def __str__( self ):
+		return self.caption
+
+
+class Question( models.Model ):
+	caption = models.CharField( null = False, blank = False, max_length = 100 )
+	rating = models.IntegerField( default = 0 )
+	text = models.TextField( null = False, blank = False )
+	tags = models.ManyToManyField( 'Tag' )
+
+	def __str__( self ):
+		return self.caption + " " + self.text
+
+
+class Answer( models.Model ):
+	rating = models.IntegerField( default = 0 )
+	text = models.TextField( null = False, blank = False )
+	question = models.ForeignKey( 'Question', null = False, blank = False, on_delete = models.CASCADE )
+
+	def __str__( self ):
+		return self.text
+
+
+class Profile( User ):
+	image = models.ImageField( max_length=100 )
+	#create custom manager based on UserManager() ?
